7b577c4d70b7949a3995ccb2bcb60cf0a14283fe
xLAVAxMASTERx <93382134+xLAVAxMASTERx@users.noreply.github.com>
2022-05-02 19:51:49 +0530

    fifyh commit

--- app.js
+++ app.js
@@ -44,14 +44,14 @@ class UI{
      result += `
      <article class="product">
                <div class="img-container">
-                   <img src=₹{product.image} alt="product" class="product-img">
-                   <button class="bag-btn" data-id=₹{product.id}>
+                   <img src=${product.image} alt="product" class="product-img">
+                   <button class="bag-btn" data-id=${product.id}>
                        <i class="fas fa-shopping-cart"></i>
                        add to cart
                    </button>
                </div> 
-               <h3>₹{product.title}</h3>
-               <h4>₹{product.price}</h4>
+               <h3>${product.title}</h3>
+               <h4>${product.price}</h4>
       </article>  
      ` ; 
    }); 
